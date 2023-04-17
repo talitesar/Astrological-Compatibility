068524a2f43589e1a0a1037311e7c3631d3e42b9
Tali Tesar <tali.tesar@gmail.com>
2023-04-16 19:16:46 -0500

    javascript

--- UI/Static/app.js
+++ UI/Static/app.js
@@ -10,7 +10,25 @@ for (let i=0;i<options.length;i++){
 
 var initSign = options[0]
 
-function initGauge() {
+function initOutcome() {
 
 }
 
+function initGauge() {
+    var gauge = [
+
+        {
+          domain: { x: [0, 1], y: [0, 1] },
+          value: data.,
+          title: { text: "Combatility Rating" },
+          type: "indicator",
+          mode: "gauge+number",
+          delta: { reference: 400 },
+          gauge: { axis: { range: [0, 100] } }
+        }
+      ];
+
+      var layout = { width: 600, height: 400 };
+      
+      Plotly.newPlot('myDiv', data, layout);
+}
